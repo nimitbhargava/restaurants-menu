5d22c0e24bc902cdf6d8698f85ee5974df927134
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-11 19:31:11 +0530

    Reduced breaklines quantities between restaurant names

--- webserver.py
+++ webserver.py
@@ -24,7 +24,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name
-                    output += "</br></br></br>"
+                    output += "</br>"
 
                 output += "</body></html>"
                 self.wfile.write(output)
