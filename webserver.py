b7f589000b9897d93bda09f3a878d9679ec4064c
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-11 18:55:33 +0530

    Creating session and connecting to DB

--- webserver.py
+++ webserver.py
@@ -5,6 +5,11 @@ from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
 
